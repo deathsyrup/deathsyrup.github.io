0fd97f3d630f8b9cc5a3bf4cc8988ed22b899d5e
deathsyrup <105470458+deathsyrup@users.noreply.github.com>
2025-04-22 21:15:05 -0400

    build1

---
+++ browser.js
@@ -0,0 +1,777 @@
+const BROWSER_JS_T = {
+    nothingFoundForXxx: query => 'Nothing found for \'{query}\''.replace('{query}', query),
+    showingFeaturedItems: 'Showing featured items',
+    showingXxxResultsForXxx: (count, query) => 'Showing {count} results for \'{query}\''.replace('{count}', count).replace('{query}', query),
+    xxxAndOthers: (xxx, othersLink) => '{xxx} and <a href="{others_link}">others</a>'.replace('{xxx}', xxx).replace('{others_link}', othersLink)
+};
+
+const LABEL_MODE = true;
+
+const ARTISTS = [
+    {
+    
+    name: 'deathsyrup',
+    url: 'deathsyrup/'
+}
+
+];
+
+const RELEASES = [
+    {
+    artists: [
+    {
+    name: 'deathsyrup',
+    url: 'deathsyrup/'
+}
+
+],
+
+    cover: 'cover_160.jpg?QCgcO60EReM',
+    title: 'Weird Waveforms IV',
+    tracks: [
+        {
+    artists: [
+    
+],
+
+    
+    number: 'I.',
+    title: '[gkmhidi]',
+    url: 'dc-ww-04/1/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'II.',
+    title: '[ibkaboe]',
+    url: 'dc-ww-04/2/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'III.',
+    title: '[jfgjhum]',
+    url: 'dc-ww-04/3/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'IV.',
+    title: '[mabmdqh]',
+    url: 'dc-ww-04/4/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'V.',
+    title: '[tdrdqjw]',
+    url: 'dc-ww-04/5/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'VI.',
+    title: '[ucakpig]',
+    url: 'dc-ww-04/6/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'VII.',
+    title: '[urdofsy]',
+    url: 'dc-ww-04/7/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'VIII.',
+    title: '[vgxxwpy]',
+    url: 'dc-ww-04/8/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'IX.',
+    title: '[yhxswjg]',
+    url: 'dc-ww-04/9/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'X.',
+    title: '[yjsrxvh]',
+    url: 'dc-ww-04/10/'
+}
+
+    ],
+    url: 'dc-ww-04/'
+}
+,
+{
+    artists: [
+    {
+    name: 'deathsyrup',
+    url: 'deathsyrup/'
+}
+
+],
+
+    cover: 'cover_160.jpg?R_8JVjW0aFQ',
+    title: 'Weird Waveforms III',
+    tracks: [
+        {
+    artists: [
+    
+],
+
+    
+    number: 'I.',
+    title: '[aumeaig]',
+    url: 'dc-ww-03/1/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'II.',
+    title: '[giieqls]',
+    url: 'dc-ww-03/2/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'III.',
+    title: '[kaerlrk]',
+    url: 'dc-ww-03/3/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'IV.',
+    title: '[kqmpcqv]',
+    url: 'dc-ww-03/4/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'V.',
+    title: '[pqrvhia]',
+    url: 'dc-ww-03/5/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'VI.',
+    title: '[qtyaynj]',
+    url: 'dc-ww-03/6/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'VII.',
+    title: '[sxijfgo]',
+    url: 'dc-ww-03/7/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'VIII.',
+    title: '[vkgkjkt]',
+    url: 'dc-ww-03/8/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'IX.',
+    title: '[wtqjrlf]',
+    url: 'dc-ww-03/9/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'X.',
+    title: '[xlqflxy]',
+    url: 'dc-ww-03/10/'
+}
+
+    ],
+    url: 'dc-ww-03/'
+}
+,
+{
+    artists: [
+    {
+    name: 'deathsyrup',
+    url: 'deathsyrup/'
+}
+
+],
+
+    cover: 'cover_160.jpg?6HOpVjgkIO4',
+    title: 'Weird Waveforms II',
+    tracks: [
+        {
+    artists: [
+    
+],
+
+    
+    number: 'I.',
+    title: '[bnwbhrb]',
+    url: 'dc-ww-02/1/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'II.',
+    title: '[dlxuque]',
+    url: 'dc-ww-02/2/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'III.',
+    title: '[irflnnq]',
+    url: 'dc-ww-02/3/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'IV.',
+    title: '[lqouvwa]',
+    url: 'dc-ww-02/4/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'V.',
+    title: '[njqgjxy]',
+    url: 'dc-ww-02/5/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'VI.',
+    title: '[oessgbh]',
+    url: 'dc-ww-02/6/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'VII.',
+    title: '[pmtrvaa]',
+    url: 'dc-ww-02/7/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'VIII.',
+    title: '[rtogfcg]',
+    url: 'dc-ww-02/8/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'IX.',
+    title: '[wnxevko]',
+    url: 'dc-ww-02/9/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'X.',
+    title: '[yejexnv]',
+    url: 'dc-ww-02/10/'
+}
+
+    ],
+    url: 'dc-ww-02/'
+}
+,
+{
+    artists: [
+    {
+    name: 'deathsyrup',
+    url: 'deathsyrup/'
+}
+
+],
+
+    cover: 'cover_160.jpg?MX8r7ZHkYlo',
+    title: 'Weird Waveforms I',
+    tracks: [
+        {
+    artists: [
+    
+],
+
+    
+    number: 'I.',
+    title: '[ahuomhy]',
+    url: 'dc-ww-01/1/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'II.',
+    title: '[biywjse]',
+    url: 'dc-ww-01/2/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'III.',
+    title: '[cvojwpo]',
+    url: 'dc-ww-01/3/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'IV.',
+    title: '[esmbsvc]',
+    url: 'dc-ww-01/4/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'V.',
+    title: '[ewwbtoo]',
+    url: 'dc-ww-01/5/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'VI.',
+    title: '[lmsgsqp]',
+    url: 'dc-ww-01/6/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'VII.',
+    title: '[qsxcshq]',
+    url: 'dc-ww-01/7/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'VIII.',
+    title: '[qxbttxo]',
+    url: 'dc-ww-01/8/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'IX.',
+    title: '[sfclyuw]',
+    url: 'dc-ww-01/9/'
+}
+,
+{
+    artists: [
+    
+],
+
+    
+    number: 'X.',
+    title: '[wnnsxlt]',
+    url: 'dc-ww-01/10/'
+}
+
+    ],
+    url: 'dc-ww-01/'
+}
+
+];
+const browser = document.querySelector('#browser');
+const browseButtonFooter = document.querySelector('footer button.browse');
+const browseButtonHeader = document.querySelector('header button.browse');
+
+const browseResults = browser.querySelector('#results');
+const closeButton = browser.querySelector('button');
+const searchField = browser.querySelector('input');
+const statusField = browser.querySelector('[role="status"]');
+
+const rootPrefix = browser.dataset.rootPrefix;
+
+function truncateArtistList(artists, othersLink)  {
+    const MAX_CHARS = 40;
+
+    if (artists.length > 2) {
+        const nameChars = artists.reduce((sum, artist) => sum + artist.name.length, 0);
+        const separatorChars = (artists.length - 1) * 2; // All separating ", " between the artists
+
+        if (nameChars + separatorChars > MAX_CHARS) {
+            // Here we have more than two artists, we have a char limit,
+            // and we cannot fit all artists within the limit, thus
+            // we truncate the list.
+
+            if (LABEL_MODE) {
+                // In label mode we show at least one artist, then as many
+                // additional ones as fit, e.g. "[artist],[artist] and
+                // more"
+                let charsUsed = 0;
+                const truncatedArtists = artists
+                    .filter(artist => {
+                        if (charsUsed === 0) {
+                            charsUsed += artist.name.length;
+                            return true;
+                        }
+
+                        charsUsed += artist.name.length;
+                        return charsUsed < MAX_CHARS;
+                    });
+
+                const rArtists = truncatedArtists
+                    .map(artist => `<a href="${rootPrefix}${artist.url}">${artist.name}</a>`)
+                    .join(", ");
+
+                return BROWSER_JS_T.xxxAndOthers(rArtists, othersLink);
+            }
+
+            // In artist mode we show only "[catalog artist] and others".
+            // Our sorting ensures the catalog artist is the first one,
+            // so we can just take that.
+            const rArtists = `<a href="${rootPrefix}${artists[0].url}">${artists[0].name}</a>`;
+
+            return BROWSER_JS_T.xxxAndOthers(rArtists, othersLink);
+        }
+    }
+
+    return artists
+        .map(artist => `<a href="${rootPrefix}${artist.url}">${artist.name}</a>`)
+        .join(", ");
+}
+
+for (const release of RELEASES) {
+    let imgRelease;
+    if (release.cover) {
+        imgRelease = document.createElement('img');
+        imgRelease.src = rootPrefix + release.url + release.cover;
+    } else {
+        imgRelease = document.createElement('img');
+        imgRelease.classList.add('procedural');
+        imgRelease.src = rootPrefix + release.url + release.coverProcedural;
+    }
+
+    const aText = document.createElement('a');
+    aText.href = rootPrefix + release.url;
+
+    const aImage = aText.cloneNode(true);
+    aImage.tabIndex = -1;
+    aImage.appendChild(imgRelease);
+
+    aText.dataset.searchable = 'true';
+    aText.textContent = release.title;
+
+    const details = document.createElement('div');
+    details.appendChild(aText);
+
+    if (release.artists) {
+        const artists = document.createElement('div');
+        artists.classList.add('artists');
+        artists.innerHTML = truncateArtistList(release.artists, `${rootPrefix}${release.url}`);
+        details.appendChild(artists);
+    }
+
+    const row = document.createElement('div');
+    row.appendChild(aImage);
+    row.appendChild(details);
+    browseResults.appendChild(row);
+
+    for (const track of release.tracks) {
+        let imgTrack;
+        if (track.cover) {
+            imgTrack = document.createElement('img');
+            imgTrack.src = rootPrefix + track.url + track.cover;
+        } else {
+            imgTrack = imgRelease.cloneNode(true);
+        }
+
+        const number = document.createElement('span');
+        number.classList.add('number');
+        number.textContent = track.number;
+
+        const aTitle = document.createElement('a');
+        aTitle.href = rootPrefix + track.url;
+
+        const aImage = aTitle.cloneNode(true);
+        aImage.tabIndex = -1;
+        aImage.appendChild(imgTrack);
+
+        aTitle.dataset.searchable = 'true';
+        aTitle.textContent = track.title;
+
+        const details = document.createElement('div');
+        details.appendChild(number);
+        details.appendChild(aTitle);
+
+        if (track.artists) {
+            const artists = document.createElement('div');
+            artists.classList.add('artists');
+            artists.innerHTML = truncateArtistList(track.artists, `${rootPrefix}${track.url}`);
+            details.appendChild(artists);
+        }
+
+        const row = document.createElement('div');
+        row.appendChild(aImage);
+        row.appendChild(details);
+        row.dataset.track = '';
+        row.style.setProperty('display', 'none');
+        browseResults.appendChild(row);
+    }
+}
+
+for (const artist of ARTISTS) {
+    const aText = document.createElement('a');
+    aText.href = rootPrefix + artist.url;
+
+    let imageArtist;
+    if (artist.image) {
+        imageArtist = document.createElement('img');
+        imageArtist.classList.add('crop');
+        imageArtist.src = rootPrefix + artist.url + artist.image;
+    } else {
+        imageArtist = document.createElement('span');
+        imageArtist.classList.add('placeholder');
+    }
+
+    const aImage = aText.cloneNode(true);
+    aImage.tabIndex = -1;
+    aImage.appendChild(imageArtist);
+
+    aText.dataset.searchable = 'true';
+    aText.textContent = artist.name;
+
+    const details = document.createElement('div');
+    details.appendChild(aText);
+
+    const row = document.createElement('div');
+    row.appendChild(aImage);
+    row.appendChild(details);
+    browseResults.appendChild(row);
+}
+
+function hideBrowser() {
+    const browseButton = browseButtonFooter.ariaExpanded === 'true'
+        ? browseButtonFooter
+        : browseButtonHeader;
+
+    browser.classList.remove('active');
+    browseButton.setAttribute('aria-expanded', 'false');
+    searchField.value = '';
+    statusField.removeAttribute('aria-label');
+    statusField.textContent = '';
+    for (const result of browseResults.children) {
+        const display = result.dataset.track === undefined;
+        result.style.setProperty('display', display ? null : 'none');
+    }
+    browseButton.focus();
+}
+
+function showBrowser(browseButton) {
+    browser.classList.add('active');
+    browseButton.setAttribute('aria-expanded', 'true');
+    searchField.focus();
+    statusField.setAttribute('aria-label', BROWSER_JS_T.showingFeaturedItems);
+    statusField.textContent = '';
+}
+
+// When the browse/search modal is open and focus moves outside the page
+// entirely (e.g. to the addressbar) but then re-enters the page, we need
+// to make sure that it returns back to the browse/search modal (instead of
+// to an obscured element in the main body)
+document.body.addEventListener('focusin', event => {
+    if (browser.classList.contains('active') && !browser.contains(event.target)) {
+        searchField.focus();
+    }
+});
+
+browser.addEventListener('focusout', event => {
+    if (browser.classList.contains('active') && event.relatedTarget && !browser.contains(event.relatedTarget)) {
+        hideBrowser();
+    }
+});
+
+browser.addEventListener('keydown', event => {
+    if (event.key === 'Escape') {
+        event.preventDefault();
+        hideBrowser();
+    }
+});
+
+browseButtonFooter.addEventListener('click', () => showBrowser(browseButtonFooter));
+browseButtonHeader.addEventListener('click', () => showBrowser(browseButtonHeader));
+
+closeButton.addEventListener('click', hideBrowser);
+
+searchField.addEventListener('input', () => {
+    const query = searchField.value.trim();
+
+    if (query.length) {
+        const regexp = new RegExp(query.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), 'i');
+        let shown = 0;
+
+        for (const element of browseResults.children) {
+            const title = element.querySelector('[data-searchable]').textContent;
+            const display = regexp.test(title);
+            element.style.setProperty('display', display ? null : 'none');
+            if (display) { shown += 1; }
+        }
+
+        if (shown === 0) {
+            statusField.removeAttribute('aria-label');
+            statusField.textContent = BROWSER_JS_T.nothingFoundForXxx(query);
+        } else {
+            statusField.setAttribute('aria-label', BROWSER_JS_T.showingXxxResultsForXxx(shown, query));
+            statusField.textContent = '';
+        }
+    } else {
+        for (const element of browseResults.children) {
+            const display = element.dataset.track === undefined;
+            element.style.setProperty('display', display ? null : 'none');
+        }
+
+        statusField.setAttribute('aria-label', BROWSER_JS_T.showingFeaturedItems);
+        statusField.textContent = '';
+    }
+});
